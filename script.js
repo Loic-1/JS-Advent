e978b6631ec0b0a6d5f6c5d9f1cd5c22511151f7
Loic VASILE <loic.vasile@stagiaire-elan.fr>
2024-11-14 15:00:03 +0100

    Made setSize more changeable

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function setSize(element) {
-    console.log("setSize called");
+function setSize(element, buffer, size) {
+    console.log("--------------- setSize called ---------------");
 
-    element.style.width = 100 + 150 * Math.random().toFixed(2) + "px";
-    element.style.height = 100 + 150 * Math.random().toFixed(2) + "px";
+    element.style.width = buffer + size * Math.random().toFixed(2) + "px";
+    element.style.height = buffer + size * Math.random().toFixed(2) + "px";
 
     console.log("Width = " + element.style.width + "\nHeight = " + element.style.height);
 }
@@ -13,13 +13,15 @@ const box = document.createElement("div");
 
 box.classList.add("box");
 
-box.innerText = "test";
+
 
 
 for (let i = 0; i < 10; i++) {
     const newBox = box.cloneNode(true);
-    
-    setSize(newBox);
+
+    newBox.innerText = "test" + (i + 1);
+
+    setSize(newBox, 100, 150);
     
     adventContainer.appendChild(newBox);
 }
