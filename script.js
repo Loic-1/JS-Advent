98ddd198e170234b8e8da94d13856a586fbea1a0
Loic-1 <loic.vasile130@gmail.com>
2024-11-29 14:07:12 +0100

    Update box

--- script.js
+++ script.js
@@ -152,7 +152,7 @@ for (let i = 1; i <= 24; i++) {
   );
   // DEBUG
 
-  newBox.style.transform = `translate(30%, ${offsetY}%)`;
+  newBox.style.transform = `translate(20%, ${offsetY}%)`;
 }
 
 // gestion du popup
